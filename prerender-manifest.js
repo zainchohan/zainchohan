88ecb3ca5f5889f49a1243abdbbc927177803781
zainchohan <143502506+zainilab@users.noreply.github.com>
2024-11-25 21:55:24 +0300

    code updated

---
+++ prerender-manifest.js
@@ -0,0 +1,1 @@
+self.__PRERENDER_MANIFEST="{\"preview\":{\"previewModeId\":\"e1e0466a9eb4cc7f898023c09fe8a473\",\"previewModeSigningKey\":\"0d380340150535930434f588508d4a21c7919b527fed6f0e46c20382093504aa\",\"previewModeEncryptionKey\":\"387516f769fe6682287eff70e0901398775ef4fbcaaa759497615fc2d8fee579\"}}"
